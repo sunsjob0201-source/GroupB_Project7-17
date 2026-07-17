fb8d56af06411050f18c18f2bb4855643eabf876
1019tbs <1019tbs@gmail.com>
2026-07-17 16:06:08 +0900

    Form DAO,java.更新

--- src/main/java/com/example/demo/model/FormContact.java
+++ src/main/java/com/example/demo/model/FormContact.java
@@ -1,76 +1,72 @@
 package com.example.demo.model;
 
 public class FormContact {
-	
-	private String customerName;
-	private String email;
-	private String phone;
-	private String subject;
-	private String message;
-	
-	public FormContact() {
 
-	}
-	
-	public FormContact(
-			String customerName,
-			String email,
-			String phone,
-			String subject,
-			String message) {
-		
-		this.customerName = customerName;
-		this.email = email;
-		this.phone = phone;
-		this.subject = subject;
-		this.message = message;
-	}
-	
-	public String getCustomerName() {
-		return customerName;
-	}
-	
-	public void setCustomerName(
-			String customerName) {
-		
-		this.customerName = customerName;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(
-		String email) {
-		
-		this.email = email;
-	}
-	
-	public String getPhone() {
-		return phone;
-	}
-	
-	public void setPhone(
-			String phone) {
-		this.phone = phone;
-	}
-	
-	public String getSubject() {
-		return subject;
-	}
-	
-	public void setSubject(
-			String subject) {
-		this.subject = subject;
-	}
-	
-	public String getMessage() {
-		return message;
-	}
-	
-	public void setMessage(
-			String message) {
-		this.message = message;
-	}
-	
+    private int contactId;
+    private String customerName;
+    private String subject;
+    private String email;
+    private String phone;
+    private String message;
+
+    public FormContact() {
+    }
+
+    public int getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(
+            String customerName) {
+
+        this.customerName = customerName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(
+            String subject) {
+
+        this.subject = subject;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(
+            String email) {
+
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(
+            String phone) {
+
+        this.phone = phone;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(
+            String message) {
+
+        this.message = message;
+    }
 }
